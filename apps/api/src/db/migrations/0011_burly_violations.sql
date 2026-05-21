1e6711453fc50f590c582e33216b01132a09fce8
pmjhaqqulyaqin <pmjhaqqulyaqin@gmail.com>
2026-05-21 10:38:21 +0800

    feat: add additional fields for buku induk form

---
+++ apps/api/src/db/migrations/0011_burly_violations.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "student_profiles" ADD COLUMN "agama" varchar(50);--> statement-breakpoint
+ALTER TABLE "student_profiles" ADD COLUMN "kewarganegaraan" varchar(50);--> statement-breakpoint
+ALTER TABLE "student_profiles" ADD COLUMN "anak_ke" integer;--> statement-breakpoint
+ALTER TABLE "student_profiles" ADD COLUMN "jumlah_saudara" integer;--> statement-breakpoint
+ALTER TABLE "student_profiles" ADD COLUMN "bahasa_sehari_hari" varchar(100);--> statement-breakpoint
+ALTER TABLE "student_profiles" ADD COLUMN "golongan_darah" varchar(10);--> statement-breakpoint
+ALTER TABLE "student_profiles" ADD COLUMN "tempat_tinggal" varchar(100);--> statement-breakpoint
+ALTER TABLE "student_profiles" ADD COLUMN "jarak_sekolah_km" varchar(50);
