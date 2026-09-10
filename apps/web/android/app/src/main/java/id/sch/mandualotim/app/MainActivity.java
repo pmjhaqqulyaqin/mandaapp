f7e43b3633c3177aba1defa2391c857eaa2e389b
pmjhaqqulyaqin <pmjhaqqulyaqin@gmail.com>
2026-09-10 20:23:02 +0800

    feat: enable pinch-to-zoom in Android APK WebView

--- apps/web/android/app/src/main/java/id/sch/mandualotim/app/MainActivity.java
+++ apps/web/android/app/src/main/java/id/sch/mandualotim/app/MainActivity.java
@@ -1,5 +1,20 @@
 package id.sch.mandualotim.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Enable pinch-to-zoom in WebView
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+        settings.setBuiltInZoomControls(true);
+        settings.setDisplayZoomControls(false); // Hide +/- buttons, keep pinch gesture
+        settings.setSupportZoom(true);
+    }
+}
